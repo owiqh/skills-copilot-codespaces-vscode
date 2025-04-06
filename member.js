6059cf34a05a24973aecab79d88228fc55be687c
owiqh <datnas52@gmail.com>
2025-04-06 10:35:17 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,18 @@
+function skillsMember() {
+  return [
+    {
+      name: "JavaScript",
+      level: "Advanced",
+    },
+    {
+      name: "React",
+      level: "Intermediate",
+    },
+    {
+      name: "Node.js",
+      level: "Intermediate",
+    },
+  ];
+}
+
+
